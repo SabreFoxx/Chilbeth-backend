55e87c6dedd5f47d157bc2f97a59bac3f3bece24
SabreFoxx <geraldnebs@gmail.com>
2021-12-22 14:20:41 +0100

    modify cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(passport.initialize()); // Initialize passport after the static paths
 app.use('/', (req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Origin', 'http://chinyereodinukwe.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
